2233264b83e7a2acae4096c5926b2f0c9019d19a
TypeDefinition <typedefinition@outlook.sg>
2024-10-20 07:38:14 -0400

    Update Workshop 1 Example 4, and uploaded slides

--- UnityWorkshopExamples/Assets/Workshop1/Example4/Scripts/W1E4Laser.cs
+++ UnityWorkshopExamples/Assets/Workshop1/Example4/Scripts/W1E4Laser.cs
@@ -5,74 +5,52 @@ using UnityEngine;
 public class W1E4Laser : MonoBehaviour
 {
     [Header("References")]
-    [SerializeField] private LineRenderer lineForward;
-    [SerializeField] private LineRenderer lineRicochet1;
-    [SerializeField] private LineRenderer lineRicochet2;
+    [SerializeField] private LineRenderer[] lines = new LineRenderer[0];
 
-    [Header("Rotation")]
-    [SerializeField] private float angularVelocity = 60.0f;
+    [Header("Settings")]
+    [SerializeField] private float angularVelocity = 5.0f;
 
     private void Start() {}
 
     private void Update() {
         transform.Rotate(Vector3.up, angularVelocity * Time.deltaTime, Space.World);
-        SetRicochetLines();
+        SetLasers();
     }
 
-    private void SetRicochetLines() {
+    private void SetLasers() {
+        const int maxLines = 3;
         const float maxDistance = 100.0f;
-        lineRicochet1.enabled = false;
-        lineRicochet2.enabled = false;
 
-        // **** Let's render a line in the forward direction of the player to the nearest surface. **** //
-        lineForward.enabled = true;
-        Vector3 startPos = transform.position;
-        Vector3 direction = transform.forward;
+        foreach (LineRenderer line in lines)
+            line.enabled = false;
 
-        // First, check for a surface in front of the player.
-        RaycastHit hit;
-        if (!Physics.Raycast(startPos, direction, out hit, maxDistance)) {
-            // If there is no surface, just render a really long straight line in the player's forward direction.
-            lineForward.SetPositions(new Vector3[] { startPos, startPos + direction * maxDistance });
-            return;
+        Vector3 startPos = transform.position; // Where the line starts.
+        Vector3 direction = transform.forward; // Where the line points towards.
+
+        // Let's render each laser segment.
+        for (int i = 0; i < maxLines; ++i) {
+            LineRenderer line = lines[i];
+            line.enabled = true;
+
+            // Check if the laser hits a surface.
+            RaycastHit hit;
+            if (!Physics.Raycast(startPos, direction, out hit, maxDistance)) {
+                // If there is no surface, just render a long straight line in the current direction.
+                line.SetPositions(new Vector3[] { startPos, startPos + direction * maxDistance });
+                break;
+            }
+            // If a surface exists, render a line from the current point to the surface.
+            line.SetPositions(new Vector3[] { startPos, hit.point });
+            
+            // Update the start position for the next line segment.
+            startPos = hit.point;
+
+            /* Find the projection of the player's forward direction onto the normal of the surface.
+             * Note that both transform.forward and hit.normal are unit vectors. */
+            Vector3 projection = Vector3.Project(direction, hit.normal);
+            /* Calculate the reflection of the ray off the surface.
+             * Note that the reflection direction will also be a unit vector. */
+            direction -= projection * 2.0f;
         }
-        // If a surface exists, render a line from the player to where the raycast hit.
-        lineForward.SetPositions(new Vector3[] { startPos, hit.point });
-        // ******************************************************************************************** //
-
-        // **** Next, let's render a line showing the direction the bullet will ricochet. **** //
-        lineRicochet1.enabled = true;
-        startPos = hit.point;
-
-        /* Find the projection of the player's forward direction onto the normal of the surface.
-         * Note that both transform.forward and hit.normal are unit vectors. */
-        Vector3 projection = Vector3.Project(transform.forward, hit.normal);
-        /* Calculate the reflection of the ray off the surface.
-         * Note that the reflection direction will also be a unit vector. */
-        direction -= projection * 2.0f;
-
-        // Check for another surface in the direction of the ricochet.
-        if (!Physics.Raycast(startPos, direction, out hit, maxDistance)) {
-            lineRicochet1.SetPositions(new Vector3[] { startPos, startPos + direction * maxDistance });
-            lineRicochet2.enabled = false;
-            return;
-        }
-        lineRicochet1.SetPositions(new Vector3[] { startPos, hit.point });
-        // ************************************************************************************ //
-
-        // **** Finally, let's render a line showing the direction the bullet will ricochet the second time. **** //
-        lineRicochet2.enabled = true;
-        startPos = hit.point;
-
-        projection = Vector3.Project(direction, hit.normal);
-        direction -= projection * 2.0f;
-        
-        // Check for another surface in the direction of the ricochet.
-        if (!Physics.Raycast(startPos, direction, out hit, maxDistance)) {
-            lineRicochet2.SetPositions(new Vector3[] { startPos, startPos + direction * maxDistance });
-            return;
-        }
-        lineRicochet2.SetPositions(new Vector3[] { startPos, hit.point });
-        // ****************************************************************************************************** //
     }
 }
